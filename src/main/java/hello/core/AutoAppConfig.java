94b02f38fbc90cd23ef633044b76a6acabab83b2
ufonetcom <ufozx@naver.com>
2021-04-08 19:57:01 +0900

    <inflearn> 컴포넌트 스캔과 의존관계 자동 주입 시작하기!! (이전까지는 수동 주입 AppConfig.java)

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+) //현재 프로젝트의 AppConfig.class(수동설정방법)이 component로 포함되있기 때문에 충돌남.. 이것을 해결
+public class AutoAppConfig {
+
+
+}
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired //ac.getBean(MemberRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
@@ -16,5 +21,11 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
+    }
+
+    //테스트 용
+
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
