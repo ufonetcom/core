86b2003f2a4a1fab8797c1ceb7716e6088df0f73
ufonetcom <ufozx@naver.com>
2021-04-08 18:06:11 +0900

    <inflearn> 싱글톤 방식의 주의점!! - 스프링 빈은 항상 무상태로 설계해야한다!!

---
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -0,0 +1,20 @@
+package hello.core.singleton;
+
+public class StatefulService {
+
+//    private int price; //상태를 유지하는 필드 10000 -> 20000원으로 변경
+
+//    public void order(String name, int price) {
+//        System.out.println("name = " + name + "price = " + price);
+//        this.price = price; //여기가 문제!
+//    }
+
+    public int order(String name, int price) {
+        System.out.println("name = " + name + "price = " + price);
+        return price;
+    }
+
+//    public int getPrice() {
+//        return price;
+//    }
+}
