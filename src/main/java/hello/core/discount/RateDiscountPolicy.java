97dd810ac85011c1f9c995ff26739ffb413daa28
ufonetcom <ufozx@naver.com>
2021-04-11 19:35:23 +0900

    <inflearn> 조회한 빈이 모두 필요할 때, List, Map

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,13 +1,15 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+//@Primary //이 어노테이션이 붙으면 최고 우선순위로 지정돼어 제일 먼저 호출된다. (<==>@Qualifier("지정이름"))
 @Component
-@Primary //이 어노테이션이 붙으면 최고 우선순위로 지정돼어 제일 먼저 호출된다. (<==>@Qualifier("지정이름"))
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
